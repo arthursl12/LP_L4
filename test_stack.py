ac9db70665615cdf0e87ffe94f33e3380289cee3
arthursl12 <arthursoutolima@gmail.com>
2021-02-23 09:38:44 -0300

    Questão 3

--- test_stack.py
+++ test_stack.py
@@ -2,7 +2,7 @@ from filecmp import cmp
 
 import pytest
 
-from q2 import Stack, Node
+from q2 import Stack, Node, Queue
 
 class TestNode:
     def test_equals(self):
@@ -23,6 +23,80 @@ class TestNode:
         assert (n2 == n1) == False
         n2.e = "1"
         assert (n2 == n1)
+
+class TestQueue:
+    def test_empty(self):
+        s = Queue()
+        assert s.isNotEmpty() == False
+        
+        s.add(1)
+        assert s.isNotEmpty() == True
+        
+        s.remove()
+        assert s.isNotEmpty() == False
+        
+    def test_add_remove(self):
+        s = Queue()
+        s.add("Baltimore")
+        s.add("Lord")
+        s.add("Sir")
+        s.remove()  # Remove Baltimore
+        s.remove()  # Remove Lord
+        s.add("TheLord")    
+        s.add("TheSir")
+        assert s.remove() == "Sir"
+        assert s.remove() == "TheLord"
+        assert s.remove() == "TheSir"
+        
+    def test_specs(self):
+        s = Queue()
+        s.add("Baltimore")
+        s.add("Lord")
+        s.add("Sir")
+        assert s.isNotEmpty()
+        
+        assert s.remove() == "Baltimore"
+        assert s.isNotEmpty()
+        
+        assert s.remove() == "Lord"
+        assert s.isNotEmpty()
+        
+        assert s.remove() == "Sir"
+        assert s.isNotEmpty() == False
+    
+    def test_getSmaller_empty(self):
+        s = Queue()
+        assert s.getSmaller() is None
+
+    def test_getSmaller_spec(self):
+        s = Queue()
+        s.add("C")
+        s.add("A")
+        s.add("B")
+        
+        assert s.isNotEmpty()
+        assert s.getSmaller() == "A"
+    
+    def test_getSmaller(self):
+        s = Queue()
+        s.add("C")
+        s.add("Y")
+        s.add("A")
+        s.add("B")
+        s.add("D")
+        s.add("K")
+        s.add("A")
+        
+        assert s.isNotEmpty()
+        assert s.getSmaller() == "A"
+    
+    def test_getSmaller_all_equal(self):
+        s = Queue()
+        s.add("ABC")
+        s.add("ABC")
+        s.add("ABC")
+        assert s.isNotEmpty()
+        assert s.getSmaller() == "ABC"
 
 class TestStack:
     def test_empty(self):
@@ -63,6 +137,7 @@ class TestStack:
         
         assert s.remove() == "Baltimore"
         assert s.isNotEmpty() == False
+
 
 class TestArquivosIguais:
     def test_compara(self):
